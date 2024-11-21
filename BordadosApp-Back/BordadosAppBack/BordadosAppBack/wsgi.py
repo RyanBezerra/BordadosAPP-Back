8a2bf12d187ec8bcbdc7051324c0cfc9bf83fdcf
Ryan Bezerra <101473672+RyanBezerra@users.noreply.github.com>
2024-11-21 14:44:55 -0300

    df

--- BordadosApp-Back/BordadosAppBack/BordadosAppBack/wsgi.py
+++ BordadosApp-Back/BordadosAppBack/BordadosAppBack/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BordadosAppBack.settings')
 
 application = get_wsgi_application()
+
+# Adicione esta linha para a Vercel
+app = application
